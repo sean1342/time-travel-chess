caa70ce30603dceb6bc81f06c3312192d15d3af5
Sean Thompson <98042267+sean1342@users.noreply.github.com>
2023-04-17 08:15:45 -0400

    working with 2d time (3x3) boards

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ square_size = 80
 white_move = True
 
 class Board:
-    def __init__(self, size=1, pos=[0, 0]):
+    def __init__(self, size=1, pos=(0, 0)):
         self.board_surface = pygame.Surface((square_size * 8 * size, square_size * 8 * size))
         self.pos = pos
         self.size = size
@@ -72,28 +72,29 @@ class Board:
         row = y // (square_size * self.size)
         return (row, col)
 
-    def copy(self):
-        b = Board()
-        b.size = self.size
-        b.pos = self.pos
-        b.board_state = self.board_state
-        return b
-
 valid_moves = []
 
 def draw(boards):
-    screen.fill((0,0,0))
     for l in boards:
         for board in l:
             board.draw()
     for (b, valid_move) in valid_moves:
-        pygame.draw.circle(screen, (0,255,7), (valid_move[0] * square_size * b.size + b.pos[0] + square_size / 2 * b.size,
-                                               valid_move[1] * square_size * b.size + b.pos[1] + square_size / 2 * b.size), 10 * b.size)
+        pygame.draw.circle(screen, (0,255,7), (valid_move[0] * square_size * b.size + b.pos[0] + square_size / 2 * b.size, valid_move[1] * square_size * b.size + b.pos[1] + square_size / 2 * b.size), 10 * b.size)
     pygame.display.update()
 
-board1 = Board(0.3, [0, 0])
+board1 = Board(0.3, (20, 20))
+board2 = Board(0.3, (220, 20))
+board3 = Board(0.3, (420, 20))
+board4 = Board(0.3, (20, 220))
+board5 = Board(0.3, (220, 220))
+board6 = Board(0.3, (420, 220))
+board7 = Board(0.3, (20, 420))
+board8 = Board(0.3, (220, 420))
+board9 = Board(0.3, (420, 420))
 
-boards = [[board1]]
+boards = [[board1, board2, board3],
+          [board4, board5, board6],
+          [board7, board8, board9]]
 
 s_board = None
 e_board = None
@@ -113,7 +114,6 @@ while running:
             b = None
             for l in boards:
                 for board in l:
-                    print(board.pos)
                     if mouse_down_pos[0] > board.pos[0] and mouse_down_pos[0] < board.pos[0] + square_size * 8 * board.size and \
                        mouse_down_pos[1] > board.pos[1] and mouse_down_pos[1] < board.pos[1] + square_size * 8 * board.size:
                         s_board = board
@@ -127,36 +127,32 @@ while running:
                                 valid_moves.append((board, (j, i)))
 
         elif event.type == pygame.MOUSEBUTTONUP:
+            valid_moves = []
+            draw(boards)
+
             mouse_up_pos = pygame.mouse.get_pos()
-            b = None
             for l in boards:
                 for board in l:
                     if mouse_up_pos[0] > board.pos[0] and mouse_up_pos[0] < board.pos[0] + square_size * 8 * board.size and \
                        mouse_up_pos[1] > board.pos[1] and mouse_up_pos[1] < board.pos[1] + square_size * 8 * board.size:
                         e_board = board
 
-            valid_moves = []
+            if not e_board:
+                break
+
             mouse_up_pos = pygame.mouse.get_pos()
             start_pos = s_board.get_square(mouse_down_pos)
             end_pos = e_board.get_square(mouse_up_pos)
 
             if is_valid_move(boards, white_move, s_board, e_board, start_pos, end_pos):
-                if s_board == e_board:
-                    new_b = s_board.copy()
-                    new_b.pos[0] += 210
-                    x = round(start_pos[0], 0)
-                    y = round(start_pos[1], 0)
-                    p = s_board.board_state[int(start_pos[0])][(int(start_pos[1]))]
-                    s_board.board_state[int(start_pos[0])][(int(start_pos[1]))] = "."
-                    x = round(end_pos[0], 0)
-                    y = round(end_pos[1], 0)
-                    e_board.board_state[int(end_pos[0])][int(end_pos[1])] = p
-                    white_move = not white_move
-                    for i, l in enumerate(boards):
-                        if s_board in l:
-                            boards[i].append(new_b)
-                else:
-                    pass
+                x = round(start_pos[0], 0)
+                y = round(start_pos[1], 0)
+                p = s_board.board_state[int(start_pos[0])][(int(start_pos[1]))]
+                s_board.board_state[int(start_pos[0])][(int(start_pos[1]))] = "."
+                x = round(end_pos[0], 0)
+                y = round(end_pos[1], 0)
+                e_board.board_state[int(end_pos[0])][int(end_pos[1])] = p
+                white_move = not white_move
 
             s_board = None
             e_board = None
